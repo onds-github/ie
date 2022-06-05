6c6ecee39f0f6003b29665d330ee967ba7d65d16
Alberto Cruz <suporte@onds.com.br>
2022-06-05 12:03:50 -0300

    main

--- application/controllers/ExploreController.php
+++ application/controllers/ExploreController.php
@@ -15,13 +15,4 @@ class ExploreController extends Zend_Controller_Action {
         $this->view->headScript();
     }
     
-    public function reviewsAction() {
-        $this->_helper->layout->disableLayout();
-        $this->_helper->viewRenderer->setNoRender();
-        $this->getResponse()->setHeader('Content-Type', 'application/json');
-        
-    
-        echo Zend_Json::encode(json_decode(file_get_contents('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJY0be9z3Xv5QRk3V4ubGfoCM&fields=review&key=AIzaSyD9rpLXC9wZovKtwvGj57jy1bo0H9SSxXI&language=pt-BR')));
-    }
-    
 }
