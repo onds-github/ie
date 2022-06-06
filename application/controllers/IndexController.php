1184bd5df61944bcd3d2cdde225eff233fa41598
Alberto Cruz <suporte@onds.com.br>
2022-06-06 10:50:05 -0300

    main

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,7 +7,6 @@ class IndexController extends Zend_Controller_Action {
     }
 
     public function indexAction() {
-        $this->_helper->layout->setLayout('layout_preview');
         $this->view->page_id = '5ed1cf34bf802c19af9f8168';
         $this->view->title_page = 'Intercâmbio 360º';
         $this->view->description_page = 'A comunidade de viajantes e contadores de histórias';
