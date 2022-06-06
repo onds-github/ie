684e2e6dba29a6d606beeb1cf462806a9a911048
Alberto Cruz <suporte@onds.com.br>
2022-06-06 10:35:13 -0300

    main

---
+++ application/controllers/SignInController.php
@@ -0,0 +1,18 @@
+<?php
+
+class SignInController extends Zend_Controller_Action {
+
+    public function init() {
+    
+    }
+
+    public function indexAction() {
+        $this->_helper->layout->setLayout('layout_admin');
+        $this->view->page_id = '5ed56fd4e5322a6a6865e18e';
+        $this->view->title_page = 'Intercâmbio 360º';
+        $this->view->description_page = 'A comunidade de viajantes e contadores de histórias';
+        
+        $this->view->headScript();
+    }
+    
+}
