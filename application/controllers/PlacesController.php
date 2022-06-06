118c83d9eb7f3e266ee2ca18899e5c9199ed9659
Alberto Cruz <suporte@onds.com.br>
2022-06-06 11:38:13 -0300

    main

--- application/controllers/PlacesController.php
+++ application/controllers/PlacesController.php
@@ -15,4 +15,13 @@ class PlacesController extends Zend_Controller_Action {
         $this->view->headScript();
     }
     
+    public function placeAction() {
+        $this->_helper->layout->setLayout('layout_preview');
+        $this->view->page_id = '5ed44447a0977f3f1e4c8ef5';
+        $this->view->title_page = 'Intercâmbio 360º';
+        $this->view->description_page = 'A comunidade de viajantes e contadores de histórias';
+        
+        $this->view->headScript();
+    }
+   
 }
