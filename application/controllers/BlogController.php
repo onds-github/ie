9d982f8968baba127e6b882fb494b5f29b1f9dff
Alberto Cruz <suporte@onds.com.br>
2022-06-06 11:18:36 -0300

    main

---
+++ application/controllers/BlogController.php
@@ -0,0 +1,36 @@
+<?php
+
+class PostController extends Zend_Controller_Action {
+
+    public function init() {
+    
+    }
+
+    public function indexAction() {
+        $this->_helper->layout->setLayout('layout_preview');
+        $this->view->page_id = '5ed56f8788d63300e5f3374c';
+        $this->view->title_page = 'Intercâmbio 360º';
+        $this->view->description_page = 'A comunidade de viajantes e contadores de histórias';
+        
+        $this->view->headScript();
+    }
+    
+    public function postAction() {
+        $this->_helper->layout->setLayout('layout_preview');
+        $this->view->page_id = '5ed56f8788d63300e5f3374c';
+        $this->view->title_page = 'Intercâmbio 360º';
+        $this->view->description_page = 'A comunidade de viajantes e contadores de histórias';
+        
+        $this->view->headScript();
+    }
+    
+    public function categoryAction() {
+        $this->_helper->layout->setLayout('layout_preview');
+        $this->view->page_id = '5ed56f8788d63300e5f3374c';
+        $this->view->title_page = 'Intercâmbio 360º';
+        $this->view->description_page = 'A comunidade de viajantes e contadores de histórias';
+        
+        $this->view->headScript();
+    }
+    
+}
