8e399776f1ace6434f28df9e53c66e9ea2aa9705
Alberto Cruz <suporte@onds.com.br>
2022-07-13 09:22:16 -0300

    Update script.default.js

--- public/assets/js/script.default.js
+++ public/assets/js/script.default.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     
     $.ajax({
         type: 'GET',
-        url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=id&playlistId=PLIluMM1HNoRo8G1g8XwMkBUg6iaCXCqag&key=AIzaSyBAMiaNLinGMif1Lz9KeJpxPXRzJi30y9M',
+        url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLIluMM1HNoRo8G1g8XwMkBUg6iaCXCqag&key=AIzaSyBAMiaNLinGMif1Lz9KeJpxPXRzJi30y9M',
         success: function (e) {
             console.log(e);
           $.each(e.items, function (i, v) {
