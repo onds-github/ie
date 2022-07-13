720c19ee20ec4e13a0b7fada6bca3ce70b77cbd2
Alberto Cruz <suporte@onds.com.br>
2022-07-13 08:32:47 -0300

    att

--- public/assets/js/script.default.js
+++ public/assets/js/script.default.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&playlists=PLIluMM1HNoRo8G1g8XwMkBUg6iaCXCqag&maxResults=10&order=date&type=video&key=AIzaSyC1DIfvUzREQz20C3gSCaMZXc9aZ1cde3E',
         success: function (e) {
           $.each(e.items, function (i, v) {
-            $('.playlist-youtube').append('<li data-color="video"><a><iframe style="min-height: 250px;" width="100%" height="100%" src="https://www.youtube.com/embed/' + v.id.videoId + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a></li>');
+            $('.playlist-youtube').append('<li><div class="uk-panel"><iframe style="min-height: 250px;" width="100%" height="100%" src="https://www.youtube.com/embed/' + v.id.videoId + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></li>');
           });
         }
       });
