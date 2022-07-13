16e41b7dc5f084319651594f3616f9a6baa12936
Alberto Cruz <suporte@onds.com.br>
2022-07-13 10:18:02 -0300

    att

--- public/assets/js/script.default.js
+++ public/assets/js/script.default.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         success: function (e) {
             console.log(e);
           $.each(e.items, function (i, v) {
-            $('.playlist-PLIluMM1HNoRo_2sf28os_XeigLqB8ZP9B').append('<li><div class="uk-panel"><a href="/blog/post?=q' + v.snippet.resourceId.videoId + '"><img src="' + v.snippet.thumbnails.standard.url + '" title="' + v.snippet.description + '" width="' + v.snippet.thumbnails.standard.width + '" height="' + v.snippet.thumbnails.standard.height + '" /></a></div></li>');
+            $('.playlist-PLIluMM1HNoRo_2sf28os_XeigLqB8ZP9B').append('<li><div class="uk-panel"><a href="/blog/post?q=' + v.snippet.resourceId.videoId + '"><img src="' + v.snippet.thumbnails.standard.url + '" title="' + v.snippet.description + '" width="' + v.snippet.thumbnails.standard.width + '" height="' + v.snippet.thumbnails.standard.height + '" /></a></div></li>');
           });
         }
       });
