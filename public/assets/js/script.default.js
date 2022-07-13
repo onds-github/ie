15ee635ff61d0aa90973d6e4ea3089913686fd31
Alberto Cruz <suporte@onds.com.br>
2022-07-13 09:26:24 -0300

    Update script.default.js

--- public/assets/js/script.default.js
+++ public/assets/js/script.default.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         success: function (e) {
             console.log(e);
           $.each(e.items, function (i, v) {
-            $('.playlist-youtube').append('<li><div class="uk-panel"><iframe style="min-height: 250px;" width="100%" height="100%" src="https://www.youtube.com/embed/' + v.id.videoId + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></li>');
+            $('.playlist-youtube').append('<li><div class="uk-panel"><img src="' + v.snippet.thumbnails.default.url + '" title="' + v.snippet.description + '" /></div></li>');
           });
         }
       });
