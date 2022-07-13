ee2469d65d198ea0a98f4508b70b6a044e6f222e
Alberto Cruz <suporte@onds.com.br>
2022-07-13 08:31:08 -0300

    att

--- public/assets/js/script.default.js
+++ public/assets/js/script.default.js
@@ -1,31 +1,13 @@
 $(document).ready(function () {
     
-    $('.on-dropdown').dropdown();
-
-    $(document).on('click', '#onButtonMenu', function () {
-        $this = $(this);
-        $.ajax({
-            type: 'POST',
-            url: '/account/user/menu',
-            data: {data: 'menu_user=true'},
-            success: function (e) {
-                if (e.menu_user) {
-                    $('.isMenuLargeMargin').addClass('isMenuSmallMargin').removeClass('isMenuLargeMargin');
-                    $('.isMenuLargeVisible').addClass('isMenuNoLargeVisible').removeClass('isMenuLargeVisible');
-                    $('.isMenuLarge').addClass('isMenuSmall').removeClass('isMenuLarge');
-                    
-                    $('.isLogoSmallVisible').css('diplay', 'block');
-                    $('.isLogoLargeVisible').css('diplay', 'none');
-                } else {
-                    $('.isMenuSmallMargin').addClass('isMenuLargeMargin').removeClass('isMenuSmallMargin');
-                    $('.isMenuNoLargeVisible').addClass('isMenuLargeVisible').removeClass('isMenuNoLargeVisible');
-                    $('.isMenuSmall').addClass('isMenuLarge').removeClass('isMenuSmall');
-                    
-                    $('.isLogoSmallVisible').css('diplay', 'none');
-                    $('.isLogoLargeVisible').css('diplay', 'block');
-                }
-            }
-        });
-    });
+    $.ajax({
+        type: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&playlists=PLIluMM1HNoRo8G1g8XwMkBUg6iaCXCqag&maxResults=10&order=date&type=video&key=AIzaSyC1DIfvUzREQz20C3gSCaMZXc9aZ1cde3E',
+        success: function (e) {
+          $.each(e.items, function (i, v) {
+            $('.playlist-youtube').append('<li data-color="video"><a><iframe style="min-height: 250px;" width="100%" height="100%" src="https://www.youtube.com/embed/' + v.id.videoId + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></a></li>');
+          });
+        }
+      });
 
 });
