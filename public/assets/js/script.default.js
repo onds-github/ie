817dfe665828d109aa8c7d64b4a17a1f84193937
Alberto Cruz <suporte@onds.com.br>
2022-07-13 08:49:10 -0300

    Update script.default.js

--- public/assets/js/script.default.js
+++ public/assets/js/script.default.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     
     $.ajax({
         type: 'GET',
-        url: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCBkNpeyvBO2TdPGVC_PsPUA&key=AIzaSyC1DIfvUzREQz20C3gSCaMZXc9aZ1cde3E',
+        url: 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCxfBgPCuIZttjanFjqRTiiQ&key=AIzaSyC1DIfvUzREQz20C3gSCaMZXc9aZ1cde3E',
         success: function (e) {
           $.each(e.items, function (i, v) {
             $('.playlist-youtube').append('<li><div class="uk-panel"><iframe style="min-height: 250px;" width="100%" height="100%" src="https://www.youtube.com/embed/' + v.id.videoId + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div></li>');
